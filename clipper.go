e505bdc1b61d31362d1df77a8a492e5d1db79107
Wincent Colaiuta <win@wincent.com>
2013-02-17 23:09:09 -0800

    Simplify handleConnection() by using io.Copy
    Let the standard library do the heavy lifting for us.
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- clipper.go
+++ clipper.go
@@ -1,25 +1,22 @@
 package main
 
 import(
+	"io"
 	"log"
 	"net"
-)
-
-const(
-	RECV_BUF_LEN = 1024
 )
 
 func main() {
 	log.Print("Starting the server")
 	listener, err := net.Listen("tcp", "127.0.0.1:8377")
 	if err != nil {
-		log.Fatal("Listen: ", err.Error())
+		log.Fatal(err.Error())
 	}
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Print("Accept: ", err.Error())
+			log.Print(err.Error())
 			return
 		}
 
@@ -28,20 +25,12 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	buf := make([]byte, RECV_BUF_LEN)
-	n, err := conn.Read(buf)
+	copied, err := io.Copy(conn, conn)
 	if err != nil {
-		log.Print("Read: ", err.Error())
-		return
-	}
-
-	log.Print("Received ", n, " bytes of data")
-	_, err = conn.Write(buf)
-	if err != nil {
-		log.Print("Write: ", err.Error())
-		return
+		log.Print(err.Error())
 	} else {
-		log.Print("Reply echoed")
-		conn.Close()
+		log.Print("Echoed ", copied, " bytes")
 	}
+	conn.Close()
+	log.Print("Connection closed")
 }
