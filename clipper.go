ab49082107b84599b5475e311d8c72dcdadcbe80
Wincent Colaiuta <win@wincent.com>
2013-02-17 22:54:52 -0800

    Use log package instead of println
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- clipper.go
+++ clipper.go
@@ -1,8 +1,8 @@
 package main
 
 import(
+	"log"
 	"net"
-	"os"
 )
 
 const(
@@ -10,17 +10,16 @@ const(
 )
 
 func main() {
-	println("Starting the server")
+	log.Print("Starting the server")
 	listener, err := net.Listen("tcp", "127.0.0.1:8377")
 	if err != nil {
-		println("error Listen: ", err.Error())
-		os.Exit(1)
+		log.Fatal("Listen: ", err.Error())
 	}
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			println("error Accept: ", err.Error())
+			log.Print("Accept: ", err.Error())
 			return
 		}
 
@@ -32,15 +31,17 @@ func HandleConnection(conn net.Conn) {
 	buf := make([]byte, RECV_BUF_LEN)
 	n, err := conn.Read(buf)
 	if err != nil {
-		println("error Read: ", err.Error())
+		log.Print("Read: ", err.Error())
 		return
 	}
-	println("Received ", n, " bytes of data: ", string(buf))
+
+	log.Print("Received ", n, " bytes of data")
 	_, err = conn.Write(buf)
 	if err != nil {
-		println("error Write: ", err.Error())
+		log.Print("Write: ", err.Error())
 		return
 	} else {
-		println("Reply echoed")
+		log.Print("Reply echoed")
+		conn.Close()
 	}
 }
